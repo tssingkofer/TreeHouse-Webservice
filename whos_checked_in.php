43a5c6e27bccc8e1e0f5e1d4efae53db4987812c
tssingkofer <tssingkofer@gmail.com>
2013-12-05 09:20:34 -0600

    New script to add ability to view check-ins

---
+++ whos_checked_in.php
@@ -0,0 +1,36 @@
+<?php
+    
+    header("Content-type: text/xml");
+    
+    $host = "localhost";
+    $user = "root";
+    $pass = "root";
+    $database = "treehouse";
+    
+    $dbs= mysql_connect($host, $user, $pass)OR die('couldnt connect to database: '. mysql_error());
+    @mysql_select_db($database)OR die('couldnt establish connection: '. mysql_error());
+	
+	$query="Select YouthID,FirstName,LastName from treehouse.dbo_youthinfo where YouthID in (SELECT YouthID FROM treehouse.dbo_attendance where date = current_date);";
+        $resultID=mysql_query($query)OR die("Query: $query\n<br />MySQL Error:".mysql_error());
+        
+        $xml_output = "<?xml version=\"1.0\"?>\n";
+
+        $xml_output .= "<results>\n";
+
+for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){
+    $row = mysql_fetch_assoc($resultID);
+    $xml_output .= "<student>";
+    $xml_output .= "\t<YouthId>" . $row['YouthId'] . "</YouthId>\n";
+    //first name
+    $xml_output .= "\t\t<FirstName>" . $row['FirstName'] . "</FirstName>\n";
+    //last name
+    $xml_output .= "\t\t<LastName>" . $row['LastName'] . "</LastName>\n";
+    //race
+    $xml_output .= "\t\t\t<DOB>" . $row['DOB'] . "</DOB>\n";
+    $xml_output .= "</student>";
+	
+$xml_output .= "</results>";
+echo $xml_output;
+
+	
+	?>
