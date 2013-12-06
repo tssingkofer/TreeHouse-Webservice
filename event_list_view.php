efac75d35c12764f2f4ef97e2b59f8e966cf409b
tssingkofer <tssingkofer@gmail.com>
2013-12-06 14:59:52 -0600

    SQL Fix
    Was breaking until I limited the number of entries to 10. We need to
    figure out how to allow more entries at some point.

--- event_list_view.php
+++ event_list_view.php
@@ -10,13 +10,15 @@
     $dbs= mysql_connect($host, $user, $pass)OR die('couldnt connect to database: '. mysql_error());
     @mysql_select_db($database)OR die('couldnt establish connection: '. mysql_error());
 	
-	$query="SELECT ProgramID,ActivityName,ProgramDate FROM treehouse.dbo_programs WHERE ActivityName IS NOT NULL";
-        $resultID=mysql_query($query)OR die("Query: $query\n<br />MySQL Error:".mysql_error());
+$query="SELECT ProgramID,ActivityName,ProgramDate FROM treehouse.dbo_Programs WHERE ActivityName IS NOT NULL ORDER BY ProgramDate LIMIT 10";        
+
+$resultID=mysql_query($query)OR die("Query: $query\n<br />MySQL Error:".mysql_error());
         
         $xml_output = "<?xml version=\"1.0\"?>\n";
 
         $xml_output .= "<results>\n";
 
+for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){
     $row = mysql_fetch_assoc($resultID);
     $xml_output .= "<student>";
     $xml_output .= "\t<ProgramID>" . $row['ProgramID'] . "</ProgramID>\n";
@@ -26,6 +28,7 @@
     $xml_output .= "\t\t<ProgramDate>" . $row['ProgramDate'] . "</ProgramDate>\n";
     
     $xml_output .= "</student>";
+}
 	
 $xml_output .= "</results>";
 echo $xml_output;
