dc86a488cc56f5860c4c3f806300b2d6b9766699
JugglerMaster <JugglerMaster@users.noreply.github.com>
2013-12-12 21:22:00 -0600

    Update youth_checkin_query.php
    Changed last to first

--- youth_checkin_query.php
+++ youth_checkin_query.php
@@ -1,43 +1,43 @@
 <?php
+
+header("Content-type: text/xml");
+
+$host = "localhost";
+$user = "root";
+$pass = "root";
+$database = "treehouse";
+$query_var = htmlspecialchars($_GET["FirstName"]);
+$submit_var = htmlspecialchars($_GET["id"]);
+
+$dbs= mysql_connect($host, $user, $pass)OR die('couldnt connect to database: '. mysql_error());
+@mysql_select_db($database)OR die('couldnt establish connection: '. mysql_error());
+
+if ($query_var != "")
+{
+    $query="SELECT YouthId, FirstName, LastName, DOB FROM dbo_youthinfo WHERE FirstName ='$query_var'"; //define level here
+    $resultID=mysql_query($query)OR die("Query: $query\n<br />MySQL Error:".mysql_error());
     
-    header("Content-type: text/xml");
+    $xml_output = "<?xml version=\"1.0\"?>\n";
     
-    $host = "localhost";
-    $user = "root";
-    $pass = "root";
-    $database = "treehouse";
-    $query_var = htmlspecialchars($_GET["LastName"]);
-    $submit_var = htmlspecialchars($_GET["id"]);
+    $xml_output .= "<results>\n";
     
-    $dbs= mysql_connect($host, $user, $pass)OR die('couldnt connect to database: '. mysql_error());
-    @mysql_select_db($database)OR die('couldnt establish connection: '. mysql_error());
+    for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){
+        $row = mysql_fetch_assoc($resultID);
+        $xml_output .= "<student>";
+        $xml_output .= "\t<YouthId>" . $row['YouthId'] . "</YouthId>\n";
+        //first name
+        $xml_output .= "\t\t<FirstName>" . $row['FirstName'] . "</FirstName>\n";
+        //last name
+        $xml_output .= "\t\t<LastName>" . $row['LastName'] . "</LastName>\n";
+        //race
+        $xml_output .= "\t\t\t<DOB>" . $row['DOB'] . "</DOB>\n";
+        $xml_output .= "</student>";
+    }
     
-    if ($query_var != "")
-    {
-        $query="SELECT YouthId, FirstName, LastName, DOB FROM dbo_youthinfo WHERE LastName ='$query_var'"; //define level here
-        $resultID=mysql_query($query)OR die("Query: $query\n<br />MySQL Error:".mysql_error());
-        
-        $xml_output = "<?xml version=\"1.0\"?>\n";
-
-        $xml_output .= "<results>\n";
-
-for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){
-    $row = mysql_fetch_assoc($resultID);
-    $xml_output .= "<student>";
-    $xml_output .= "\t<YouthId>" . $row['YouthId'] . "</YouthId>\n";
-    //first name
-    $xml_output .= "\t\t<FirstName>" . $row['FirstName'] . "</FirstName>\n";
-    //last name
-    $xml_output .= "\t\t<LastName>" . $row['LastName'] . "</LastName>\n";
-    //race
-    $xml_output .= "\t\t\t<DOB>" . $row['DOB'] . "</DOB>\n";
-    $xml_output .= "</student>";
-}
-
-$xml_output .= "</results>";
-
-echo $xml_output;
-
+    $xml_output .= "</results>";
+    
+    echo $xml_output;
+    
 } else
 {
     if ($submit_var != "")
@@ -45,14 +45,13 @@ echo $xml_output;
         $query2 ="CREATE TABLE IF NOT EXISTS dbo_attendance (`id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY, `YouthID` INT( 11 ) NOT NULL COMMENT 'foreign key',`date` DATETIME NOT NULL ,INDEX ( `YouthID` ),
         FOREIGN KEY (YouthID) REFERENCES dbo_youthinfo(YouthID) ON UPDATE CASCADE ON DELETE CASCADE)";
         //ALTER TABLE `dbo_attendance` ADD FOREIGN KEY ( `YouthID` ) REFERENCES `treehouse`.`dbo_youthinfo` (`YouthID`) ON DELETE CASCADE ON UPDATE CASCADE ;
-        $query="INSERT INTO `treehouse`.`dbo_attendance` (`id` ,`YouthID` ,`date`)VALUES (NULL , '$submit_var', date('Y-m-d H:i:s'))"; //define level here
+        $query="INSERT INTO `treehouse`.`dbo_attendance` (`id` ,`YouthID` ,`date`)VALUES (NULL , '$submit_var', NOW())"; //define level here
         $requltID2=mysql_query($query2)OR die("Query: $query2\n<br />MySQL Error:".mysql_error());
         $resultID=mysql_query($query)OR die("Query: $query\n<br />MySQL Error:".mysql_error());
         
         
         /*$xml_output = "<?xml version=\"1.0\"?>\n";
          $xml_output .= "<results>\n";
-         
          $row = mysql_fetch_assoc($resultID);
          $xml_output .= "\t<YouthId>" . "" . "</YouthId>\n";
          $xml_output .= "</results>"; */
