785b3611da93aea2dbadc8ea308a7199d680638f
JugglerMaster <JugglerMaster@users.noreply.github.com>
2014-02-07 11:59:41 -0600

    Update youth_checkin_query.php
    Updated xml encoding to work better with different characters

--- youth_checkin_query.php
+++ youth_checkin_query.php
@@ -17,7 +17,7 @@ if ($query_var != "")
     $query="SELECT YouthId, FirstName, LastName, DATE_FORMAT(dob,'%m-%d-%Y') as DOB FROM dbo_youthinfo WHERE FirstName ='$query_var'"; //define level here
     $resultID=mysql_query($query)OR die("Query: $query\n<br />MySQL Error:".mysql_error());
     
-    $xml_output = "<?xml version=\"1.0\"?>\n";
+    $xml_output = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n";
     
     $xml_output .= "<results>\n";
     
